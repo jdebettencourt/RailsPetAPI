44f6a5e2344b87e5a15b28a376bc0553e99dd368
Lior Bendat <liorbendat@gmail.com>
2013-08-26 18:44:49 -0700

    added migrations to pet model and deleted wrong migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130827012246) do
+ActiveRecord::Schema.define(:version => 20130827014428) do
 
   create_table "pet_clients", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(:version => 20130827012246) do
   create_table "pets", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "animal"
   end
 
 end
