97a9d42f95f8754e137d04830b936ab3a041d016
Lior Bendat <liorbendat@gmail.com>
2013-08-26 18:12:48 -0700

    initial commit - empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenderpets::Application.config.secret_token = 'db3c1c9183d400234898edc651f215c54adcfc2c898c97787afe14b74d15bd11f02755f1547b6b73f4debdf41b7b4675fadcc0eadb7b8e783794f86ba9e2e11b'
